bb8c15d8a68a95b7db8ddceeea4e3eff988b5514
Rira1011 <rira1011@h-ka.de>
2025-09-29 14:52:22 +0200

    feat: Enhance EDA: Add data understanding and data_loader class

---
+++ notebooks/utils/data_loader.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+
+class DataLoader:
+    def __init__(self):
+        pass  
+    
+    def load_transform(self, path):
+        df = pd.read_csv(path)
+        df["time"] = pd.to_datetime(df["time"], format="%Y-%m-%d") 
+        df = df.sort_values(["id", "time"])
+        return df
+    
+    def date_feature_extraction(self, df):
+        df = df.copy()
+        df["month_num"] = df["time"].dt.month
+        df["year"] = df["time"].dt.year
+        seasons = np.array(["Winter", "Spring", "Summer", "Autumn"])
+        df["season"] = seasons[((df["month_num"] % 12) // 3)]
+        df["date_diff"] = df.groupby("id")["time"].diff().dt.days
+        return df
+
