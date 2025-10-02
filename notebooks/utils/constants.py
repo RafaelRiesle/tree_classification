5ed61eecd3b8356773619368d2a62b56e1b6629b
Rira1011 <rira1011@h-ka.de>
2025-10-02 13:03:50 +0200

    feat: add anomaly detection

--- notebooks/utils/constants.py
+++ notebooks/utils/constants.py
@@ -1,2 +1,13 @@
-spectral_bands = ['b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'b8a', 'b11', 'b12']
-indices = ['ndvi', 'gndvi', 'wdvi','tndvi', 'savi', 'ipvi', 'mcari', 'reip', 'masvi2', 'dvi']
+spectral_bands = ["b2", "b3", "b4", "b5", "b6", "b7", "b8", "b8a", "b11", "b12"]
+indices = [
+    "ndvi",
+    "gndvi",
+    "wdvi",
+    "tndvi",
+    "savi",
+    "ipvi",
+    "mcari",
+    "reip",
+    "masvi2",
+    "dvi",
+]
--- notebooks/utils/data_loader.py
+++ notebooks/utils/data_loader.py
@@ -1,16 +1,17 @@
 import numpy as np
 import pandas as pd
 
+
 class DataLoader:
     def __init__(self):
-        pass  
-    
+        pass
+
     def load_transform(self, path):
         df = pd.read_csv(path)
-        df["time"] = pd.to_datetime(df["time"], format="%Y-%m-%d") 
+        df["time"] = pd.to_datetime(df["time"], format="%Y-%m-%d")
         df = df.sort_values(["id", "time"])
         return df
-    
+
     def date_feature_extraction(self, df):
         df = df.copy()
         df["month_num"] = df["time"].dt.month
@@ -20,6 +21,8 @@ class DataLoader:
         df["date_diff"] = df.groupby("id")["time"].diff().dt.days
         return df
 
-    def feature_extraction(self,df):
-        df["is_disturbed"] = df["disturbance_year"].apply(lambda x: False if x == 0 else True)
+    def feature_extraction(self, df):
+        df["is_disturbed"] = df["disturbance_year"].apply(
+            lambda x: False if x == 0 else True
+        )
         return df
