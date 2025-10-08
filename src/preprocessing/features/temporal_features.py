03c9b3fe5ada24d8f797c27cb851f3cf3fd7e388
alex-c001 <alex.cozma19@gmail.com>
2025-10-08 15:34:52 +0200

    feat: more temporal features

--- src/preprocessing/features/temporal_features.py
+++ src/preprocessing/features/temporal_features.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 class TemporalFeatures:
     """
@@ -18,13 +19,21 @@ class TemporalFeatures:
         df["season"] = df["month_num"].apply(lambda m: seasons[((m % 12) // 3)])
 
         return df
+    
+    def month_sin_cos(self, df: pd.DataFrame) -> pd.DataFrame:
+        df = df.copy()
+        df["month_num"] = df["time"].dt.month
+        df["month_sin"] = np.sin(2 * np.pi * df["month"] / 12)
+        df["month_cos"] = np.cos(2 * np.pi * df["month"] / 12)
+        return df
 
-    def date_diff(self, df: pd.DataFrame) -> pd.DataFrame:
+    def date_diff(self, df: pd.DataFrame) -> pd.DataFrame:  
         df = df.copy()
         df["date_diff"] = df.groupby("id")["time"].diff().dt.days
         return df
 
     def run(self, df: pd.DataFrame) -> pd.DataFrame:
         df = self.month_and_season(df)
+        df = self.month_sin_cos
         df = self.date_diff(df)
         return df
