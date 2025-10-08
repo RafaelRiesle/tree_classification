b8b7c7f8d48da108093f369406af8d2159eab9f2
reev1011 <reev1011@h-ka.de>
2025-10-08 23:07:04 +0200

    fix: :ambulance: function month_sin_cos

--- src/preprocessing/features/temporal_features.py
+++ src/preprocessing/features/temporal_features.py
@@ -23,8 +23,8 @@ class TemporalFeatures:
     def month_sin_cos(self, df: pd.DataFrame) -> pd.DataFrame:
         df = df.copy()
         df["month_num"] = df["time"].dt.month
-        df["month_sin"] = np.sin(2 * np.pi * df["month"] / 12)
-        df["month_cos"] = np.cos(2 * np.pi * df["month"] / 12)
+        df["month_sin"] = np.sin(2 * np.pi * df["month_num"] / 12)
+        df["month_cos"] = np.cos(2 * np.pi * df["month_num"] / 12)
         return df
 
     def date_diff(self, df: pd.DataFrame) -> pd.DataFrame:  
@@ -34,6 +34,6 @@ class TemporalFeatures:
 
     def run(self, df: pd.DataFrame) -> pd.DataFrame:
         df = self.month_and_season(df)
-        df = self.month_sin_cos
+        df = self.month_sin_cos(df)
         df = self.date_diff(df)
         return df
