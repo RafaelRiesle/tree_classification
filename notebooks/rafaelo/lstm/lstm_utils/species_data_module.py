ee919b05f212996b26b1bdbe72c4770d9924477b
Rira1011 <rira1011@h-ka.de>
2025-10-09 10:04:43 +0200

    refactor: Add functions to utils folder

--- notebooks/rafaelo/lstm/lstm_utils/species_data_module.py
+++ notebooks/rafaelo/lstm/lstm_utils/species_data_module.py
@@ -6,72 +6,33 @@ import torch
 
 pl.seed_everything(42)
 
-
-# class SpeciesDataModule(pl.LightningDataModule):
-#     def __init__(self, train_sequences, test_sequences, batch_size):
-#         super().__init__()
-#         self.train_sequences = train_sequences
-#         self.test_sequences = test_sequences
-#         self.batch_size = batch_size
-
-#     def setup(self, stage=None):
-#         self.train_dataset = SpeciesDataset(self.train_sequences)
-#         self.test_dataset = SpeciesDataset(self.test_sequences)
-
-
-# def train_dataloader(self):
-#     return DataLoader(
-#         self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=4
-#     )
-
-# def val_dataloader(self):
-#     return DataLoader(
-#         self.test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=4
-#     )
-
-# def test_dataloader(self):
-#     return DataLoader(
-#         self.test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=4
-#     )
-
-
 class SpeciesDataModule(pl.LightningDataModule):
-    def __init__(self, train_sequences, test_sequences, batch_size):
+    def __init__(self, train_sequences, val_sequences, test_sequences, batch_size=64):
         super().__init__()
         self.train_sequences = train_sequences
+        self.val_sequences = val_sequences
         self.test_sequences = test_sequences
         self.batch_size = batch_size
 
     def setup(self, stage=None):
         self.train_dataset = SpeciesDataset(self.train_sequences)
+        self.val_dataset = SpeciesDataset(self.val_sequences)
         self.test_dataset = SpeciesDataset(self.test_sequences)
 
-    def collate_fn(self, batch):
-        sequences = [item["sequence"] for item in batch]
-        labels = torch.tensor([item["label"] for item in batch])
-        padded_sequences = pad_sequence(sequences, batch_first=True, padding_value=0.0)
-        return {"sequence": padded_sequences, "label": labels}
-
     def train_dataloader(self):
-        return DataLoader(
-            self.train_dataset,
-            batch_size=self.batch_size,
-            shuffle=True,
-            collate_fn=self.collate_fn,
+        return torch.utils.data.DataLoader(
+            self.train_dataset, batch_size=self.batch_size, shuffle=True,
+            num_workers=4, persistent_workers=True
         )
 
     def val_dataloader(self):
-        return DataLoader(
-            self.test_dataset,
-            batch_size=self.batch_size,
-            shuffle=False,
-            collate_fn=self.collate_fn,
+        return torch.utils.data.DataLoader(
+            self.val_dataset, batch_size=self.batch_size, shuffle=False,
+            num_workers=4, persistent_workers=True
         )
 
     def test_dataloader(self):
-        return DataLoader(
-            self.test_dataset,
-            batch_size=self.batch_size,
-            shuffle=False,
-            collate_fn=self.collate_fn,
+        return torch.utils.data.DataLoader(
+            self.test_dataset, batch_size=self.batch_size, shuffle=False,
+            num_workers=4, persistent_workers=True
         )
--- notebooks/rafaelo/lstm/lstm_utils/species_predictor.py
+++ notebooks/rafaelo/lstm/lstm_utils/species_predictor.py
@@ -5,21 +5,18 @@ import torch.optim as optim
 from torchmetrics import Accuracy
 from lstm_utils.sequence_model import SequenceModel
 
-
 class SpeciesPredictor(pl.LightningModule):
-    def __init__(self, n_features: int, n_classes: int, lr: float = 1e-4):
+    def __init__(self, n_features, n_classes, lr=1e-4):
         super().__init__()
-        self.save_hyperparameters()  # speichert Parameter automatisch
+        self.save_hyperparameters()
         self.model = SequenceModel(n_features, n_classes)
         self.criterion = nn.CrossEntropyLoss()
         self.lr = lr
-
-        # TorchMetrics Accuracy
         self.train_acc = Accuracy(task="multiclass", num_classes=n_classes)
         self.val_acc = Accuracy(task="multiclass", num_classes=n_classes)
         self.test_acc = Accuracy(task="multiclass", num_classes=n_classes)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x):
         return self.model(x)
 
     def training_step(self, batch, batch_idx):
@@ -29,9 +26,8 @@ class SpeciesPredictor(pl.LightningModule):
         loss = self.criterion(outputs, labels)
         preds = torch.argmax(outputs, dim=1)
         acc = self.train_acc(preds, labels)
-
         self.log("train_loss", loss, prog_bar=True)
-        self.log("train_accuracy", acc, prog_bar=True)
+        self.log("train_acc", acc, prog_bar=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
@@ -41,9 +37,8 @@ class SpeciesPredictor(pl.LightningModule):
         loss = self.criterion(outputs, labels)
         preds = torch.argmax(outputs, dim=1)
         acc = self.val_acc(preds, labels)
-
         self.log("val_loss", loss, prog_bar=True)
-        self.log("val_accuracy", acc, prog_bar=True)
+        self.log("val_acc", acc, prog_bar=True)
 
     def test_step(self, batch, batch_idx):
         sequences = batch["sequence"]
@@ -52,9 +47,8 @@ class SpeciesPredictor(pl.LightningModule):
         loss = self.criterion(outputs, labels)
         preds = torch.argmax(outputs, dim=1)
         acc = self.test_acc(preds, labels)
-
         self.log("test_loss", loss, prog_bar=True)
-        self.log("test_accuracy", acc, prog_bar=True)
+        self.log("test_acc", acc, prog_bar=True)
 
     def configure_optimizers(self):
         return optim.Adam(self.parameters(), lr=self.lr)
