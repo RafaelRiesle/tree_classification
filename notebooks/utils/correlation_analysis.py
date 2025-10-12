3dbc76a1e5c3b8f34035f15425b7a9c81a0cdd3f
reev1011 <reev1011@h-ka.de>
2025-10-12 22:14:45 +0200

    feat: visualize functions for detect disturbed trees

--- notebooks/utils/correlation_analysis.py
+++ notebooks/utils/correlation_analysis.py
@@ -53,3 +53,33 @@ class CorrelationAnalysis:
         plt.xticks(rotation=45, ha="right")
         plt.yticks(rotation=0)
         plt.show()
+
+    def get_correlations_with_target(self, target_col: str):
+        corr_series = self.df.corr(numeric_only=True)[target_col].drop(target_col)
+        corr_series = corr_series.sort_values(ascending=False)
+
+        df_corr = pd.DataFrame({
+            "feature": corr_series.index,
+            "correlation": corr_series.values
+        })
+
+        df_corr["abs_corr"] = df_corr["correlation"].abs()
+
+        return df_corr
+    
+    def plot_correlations_with_target(self, df_corr, top_n: int):
+        df_corr = df_corr.sort_values(by="abs_corr", ascending=True).tail(top_n)
+        df_corr = df_corr.reset_index(drop=True)
+        colors = [
+            "orange" if val < 0 else "royalblue"
+            for val in df_corr["correlation"]
+        ]
+
+        plt.figure(figsize=(10, 7))
+        plt.barh(df_corr["feature"], df_corr["abs_corr"], color=colors,)
+
+        plt.xlabel("Correlation")
+        plt.ylabel("Feature")
+        plt.title(f"Top {top_n} Correlations\n(orange = negative, blue = positive)")
+        plt.grid(axis="x", linestyle="--", alpha=0.7)
+        plt.show()
--- notebooks/utils/visualization_utils/visualization_time_series.py
+++ notebooks/utils/visualization_utils/visualization_time_series.py
@@ -1,5 +1,6 @@
 from utils.constants import COLOR
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def plot_date_diff_distribution(df, column="date_diff", bins=20, color=COLOR):
@@ -23,4 +24,28 @@ def plot_date_diff_distribution(df, column="date_diff", bins=20, color=COLOR):
     plt.legend()
     plt.grid(True)
     plt.tight_layout()
+    plt.show()
+
+
+def plot_timeseries_oneid(df, id, col, df_yearly = None, yearly_mean=True, df_scaled = True):
+    df_sub = df[df["id"] == id]
+
+    plt.plot(df_sub["time"], df_sub[col], label="all data")
+
+    if yearly_mean:
+        df_yearly_sub = df_yearly[df_yearly["id"] == id].copy()
+        df_yearly_sub["year_dt"] = pd.to_datetime(
+            df_yearly_sub["year"].astype(str) + "-01-01"
+        )
+        plt.plot(df_yearly_sub["year_dt"], df_yearly_sub[col], label="Yearly mean")
+    
+    if df_scaled:
+        plt.title(f"{col} (normalized) Timeseries for ID {id}")
+    else:
+        plt.title(f"{col} Timeseries for ID {id}")
+    
+    plt.xlabel("Time")
+    plt.ylabel(col)
+    plt.legend()
+    plt.grid(True)
     plt.show()
