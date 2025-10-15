b107f4b8a01fd2a728a0015ceb586d586d0f6583
reev1011 <reev1011@h-ka.de>
2025-10-15 14:57:34 +0200

    feat: delete doy

--- src/pipelines/processing/features/basic_features.py
+++ src/pipelines/processing/features/basic_features.py
@@ -12,7 +12,16 @@ class BasicFeatures:
             df["is_disturbed"] = df["disturbance_year"].apply(lambda x: x != 0)
         return df
     
+    def delete_doy(self, df: pd.DataFrame) -> pd.DataFrame:
+        df = df.copy()
+        if "doy" in df.columns:
+            df = df.drop(columns=["doy"])
+        return df
+    
     def run(self, df):
         if not self.on:
             return df
-        return self.add_disturbance_flag(df)
+        df = self.add_disturbance_flag(df)
+        df = self.delete_doy(df)
+        return df
+    
