e57aa394e88649604db683d33fda8f232a0788be
reev1011 <reev1011@h-ka.de>
2025-10-16 13:45:52 +0200

    style: added TODO

--- src/pipelines/processing/processing_steps/interpolate_nans.py
+++ src/pipelines/processing/processing_steps/interpolate_nans.py
@@ -1,15 +1,17 @@
 import pandas as pd
 
 class InterpolateNaNs:
-    def __init__(self, on=True, method="linear"):
+    def __init__(self, on=True, method="linear", threshold=150):
         self.on = on
         self.method = method
+        self.threshold = threshold
 
-    def _interpolate_group(self, group: pd.DataFrame) -> pd.DataFrame:
+    def interpolate_group(self, group: pd.DataFrame) -> pd.DataFrame:
         numeric_cols = group.select_dtypes(include='number').columns
         group[numeric_cols] = group[numeric_cols].interpolate(method=self.method)
         return group
 
+    # TODO: interpolation only for size(id) > threshold
     def run(self, df: pd.DataFrame) -> pd.DataFrame:
 
         if not self.on:
@@ -17,7 +19,7 @@ class InterpolateNaNs:
 
         result = (
             df.groupby('id', group_keys=False)
-              .apply(self._interpolate_group)
+              .apply(self.interpolate_group)
               .reset_index(drop=True)
         )
         return result
