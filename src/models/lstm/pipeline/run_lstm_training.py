e00252f81578ce629f95002c316174c4e410baa6
rafael.riesle <rafaelriesle@gmail.com>
2025-10-17 18:01:47 +0200

    feat: Build new training_pipeline.py

--- src/models/lstm/pipeline/run_lstm_training.py
+++ src/models/lstm/pipeline/run_lstm_training.py
@@ -1,16 +1,24 @@
 from pathlib import Path
-from models.lstm.pipeline.data_processor import DataProcessor 
+from models.lstm.pipeline.data_processor import DataProcessor
 from models.lstm.experiments.lstm_trainer import LSTMTrainer
 from models.lstm.evaluation.model_evaluator import ModelEvaluator
 
 BASE_DIR = Path(__file__).parents[4]
 TRAIN_PATH = BASE_DIR / "data/processed/trainset.csv"
-VAL_PATH = BASE_DIR / "data/processed/testset.csv"
-TEST_PATH = BASE_DIR / "data/processed/valset.csv"
+TEST_PATH = BASE_DIR / "data/processed/testset.csv"
+VAL_PATH = BASE_DIR / "data/processed/valset.csv"
 
-def run():
+
+def train_model(
+    train_path=TRAIN_PATH,
+    val_path=TEST_PATH,
+    test_path=VAL_PATH,
+    batch_size=16,
+    lr=0.001,
+    max_epochs=5,
+):
     # 1. Process data
-    processor = DataProcessor(TRAIN_PATH, VAL_PATH, TEST_PATH, label_column="species")
+    processor = DataProcessor(train_path, val_path, test_path, label_column="species")
     data_info = processor.run()
 
     # 2. Train LSTM
@@ -21,21 +29,31 @@ def run():
         n_features=len(data_info["feature_columns"]),
         n_classes=data_info["n_classes"],
         class_weights=data_info["class_weights"],
-        batch_size=16,
-        lr=0.001,
-        max_epochs=5,
+        batch_size=batch_size,
+        lr=lr,
+        max_epochs=max_epochs,
     )
     trainer.train()
     model, data_module = trainer.get_model()
 
-    # 3. Evaluate model
+    print("Finished Training.")
+    return model, data_module, data_info
+
+
+def evaluate_model(model, data_module, data_info):
     evaluator = ModelEvaluator(
         model, data_module, feature_columns=data_info["feature_columns"]
     )
     evaluator.plot_training_history()
     evaluator.evaluate_confusion_matrix()
     evaluator.permutation_importance()
-    print("LSTM training and evaluation complete.")
+    print("Finished Evaluation.")
+
+
+def run():
+    model, data_module, data_info = train_model()
+    evaluate_model(model, data_module, data_info)
+    print("Finished LSTM training and evaluation.")
 
 
 if __name__ == "__main__":
