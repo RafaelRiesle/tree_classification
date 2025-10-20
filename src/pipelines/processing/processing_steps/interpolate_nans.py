b8f6d5f91f64e160bb88e8b40ad4cac582080b56
reev1011 <reev1011@h-ka.de>
2025-10-20 09:48:35 +0200

    style: changed function name

--- src/pipelines/processing/processing_steps/interpolate_nans.py
+++ src/pipelines/processing/processing_steps/interpolate_nans.py
@@ -7,7 +7,7 @@ class InterpolateNaNs:
         self.method = method
         self.threshold = threshold
 
-    def _interpolate_group(self, group: pd.DataFrame) -> pd.DataFrame:
+    def interpolate_group(self, group: pd.DataFrame) -> pd.DataFrame:
         numeric_cols = group.select_dtypes(include="number").columns
         group[numeric_cols] = group[numeric_cols].interpolate(method=self.method)
         return group
@@ -19,7 +19,7 @@ class InterpolateNaNs:
 
         result = (
             df.groupby("id", group_keys=False)
-            .apply(self._interpolate_group)
+            .apply(self.interpolate_group)
             .reset_index(drop=True)
         )
         return result
